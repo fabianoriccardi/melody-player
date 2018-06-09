54461baeb421512be39d78ff4846444b470fb672
Fabiano Riccardi <fabiuz4@hotmail.it>
2018-06-09 22:54:31 +0200

    Added the async playing, more error messages
    Signed-off-by: Fabiano Riccardi <fabiuz4@hotmail.it>

--- melody_manager.cpp
+++ melody_manager.cpp
@@ -1,4 +1,5 @@
 #include "melody_manager.h"
+#include <Ticker.h>
 
 bool Melody::loadTest(){
   Serial.println("Init melody... ");
@@ -15,18 +16,21 @@ bool Melody::loadTest(){
   return true;
 }
 
+void removeCarriageReturn(String& s){
+  if(s.charAt(s.length()-1)=='\r'){
+    s=s.substring(0,s.length()-1);
+  }
+}
+
 bool Melody::load(String fileName){
-  Serial.println("Init melody... ");
   File f=SPIFFS.open(fileName,"r");
   bool error=false;
   if(f){
-    int fileSize=f.size();
     String line;
     
     line=f.readStringUntil('\n');
-    fileSize=fileSize-line.length()-1;
-    line=line.substring(0,line.length()-1);
-    Serial.println(String("Reading line:--") + line + "-- LEn:" + line.length());
+    removeCarriageReturn(line);
+    if(debug) Serial.println(String("Reading line:--") + line + "-- Len:" + line.length());
     if(line.substring(0,5)=="title"){
         String title=line.substring(6);
         this->title=title;
@@ -35,77 +39,80 @@ bool Melody::load(String fileName){
     }
 
     line=f.readStringUntil('\n');
-    fileSize=fileSize-line.length()-1;
-    line=line.substring(0,line.length()-1);
-    Serial.println(String("Reading line:--") + line + "-- LEn:" + line.length());
+    removeCarriageReturn(line);
+    if(debug) Serial.println(String("Reading line:--") + line + "-- Len:" + line.length());
     if(!error && line.substring(0,5)=="tempo"){
          String t=line.substring(6);
          this->tempo=t.toInt();
-         Serial.println(this->tempo);
     }else{
       error=true;
     }
-
+    if(debug) Serial.println(this->tempo);
+    
     line=f.readStringUntil('\n');
-    fileSize=fileSize-line.length()-1;
-    line=line.substring(0,line.length()-1);
-    Serial.println(String("Reading line:--") + line + "-- LEn:" + line.length());
+    removeCarriageReturn(line);
+    if(debug) Serial.println(String("Reading line:--") + line + "-- Len:" + line.length());
     if(line.substring(0,6)=="length"){
         String len=line.substring(7);
         this->nNotes=len.toInt();
     }else{
       error=true; 
     }
-    Serial.println(this->nNotes);
+    if(debug) Serial.println(this->nNotes);
 
     String format;
     line=f.readStringUntil('\n');
-    fileSize=fileSize-line.length()-1;
-    line=line.substring(0,line.length()-1);
-    Serial.println(String("Reading line:--") + line + "-- LEn:" + line.length());
+    removeCarriageReturn(line);
+    if(debug) Serial.println(String("Reading line:--") + line + "-- Len:" + line.length());
     if(!error && line.substring(0,6)=="format"){
       format=line.substring(7);
     }else{
       error=true;
     }
-    Serial.println(format);
+    if(debug) Serial.println(format);
 
     if(!error){
-      nd=(NoteDuration*)malloc(sizeof(NoteDuration)*nNotes);
-      int i=0;
-      while(f.available() && i<nNotes && !error){
-        // let's try to read a couple
-        String couple=f.readStringUntil('|');
-        Serial.println(couple);
-        
-        String aux;
-        int j=0;
-        // Get the freq
-        while(j<couple.length() && couple.charAt(j)!=','){
-          aux+=couple.charAt(j);
+      if(debug) Serial.println(String("This melody object will take at least: ") + sizeof(NoteDuration)*nNotes + "bytes");
+      if(nNotes<maxLength){
+        nd=(NoteDuration*)malloc(sizeof(NoteDuration)*nNotes);
+        int i=0;
+        while(f.available() && i<nNotes && !error){
+          // let's try to read a couple
+          String couple=f.readStringUntil('|');
+          if(debug) Serial.println(couple);
+          
+          String aux;
+          int j=0;
+          // Get the freq
+          while(j<couple.length() && couple.charAt(j)!=','){
+            aux+=couple.charAt(j);
+            j++;
+          }
+          
+          if(format=="string"){
+            std::string mystr(aux.c_str());
+            nd[i].frequency = m[mystr];
+          }else if(format=="integer"){
+            nd[i].frequency = aux.toInt();
+          }else{
+            error=true;
+          }
+          if(debug) Serial.println(nd[i].frequency);
+  
           j++;
+          aux="";
+          while(j<couple.length()){
+            aux+=couple.charAt(j);
+            j++;
+          }
+          nd[i].duration = aux.toInt();
+          if(debug) Serial.println(nd[i].duration);
+          
+          i++;
         }
-        
-        if(format=="string"){
-          std::string mystr(aux.c_str());
-          nd[i].frequency = m[mystr];
-        }else if(format=="integer"){
-          nd[i].frequency = aux.toInt();
-        }else{
-          error=true;
-        }
-        Serial.println(nd[i].frequency);
-
-        j++;
-        aux="";
-        while(j<couple.length()){
-          aux+=couple.charAt(j);
-          j++;
-        }
-        nd[i].duration = aux.toInt();
-        Serial.println(nd[i].duration);
-        
-        i++;
+      }else{
+        Serial.println("Melody length too high, the load is not executed!");
+        error=true;
       }
     }
   }else{
@@ -116,16 +123,54 @@ bool Melody::load(String fileName){
     return false;
   }
   
-  Serial.println("Done!");
   return true;
 }
 
 void Melody::playSync(){
+  if(!checkReadyness()){
+    return;
+  }
   for(int i=0;i<nNotes;i++){
-    Serial.println(nd[i].frequency);
-    Serial.println(nd[i].duration);
-    tone(D6,nd[i].frequency,tempo*nd[i].duration);
+    if(debug) Serial.println(String("Playing: freq=") + nd[i].frequency + " dur=" + nd[i].duration);
+    tone(pin,nd[i].frequency,tempo*nd[i].duration);
     delay(1.30f * (tempo*nd[i].duration));
   }
+}
+
+static Ticker ticker; 
+
+void changeFreq(Melody* melody){
+  static int i=0;
+  if(i<melody->nNotes){
+    if(melody->debug) Serial.println(String("Playing async: freq=") + melody->nd[i].frequency + " dur=" + melody->nd[i].duration + " iteration=" + i);
+    tone(melody->pin,melody->nd[i].frequency,melody->tempo*melody->nd[i].duration);
+    ticker.once_ms(1.30f*melody->nd[i].duration*melody->tempo,changeFreq,melody);
+    i++;
+  }else{
+    i=0;
+    melody->state=2;
+  }
+}
+
+void Melody::playAsync(){
+  state=1;
+  ticker.once(0,changeFreq,this);
+}
+
+bool Melody::isPlaying(){
+  if(state==1){
+    return true;
+  }
+  return false;
+}
+
+bool Melody::checkReadyness(){
+  if(pin==-1){
+    return false;
+  }
+  if(nNotes<1){
+    return false;
+  }
+  return true;
 }
 
--- melody_manager.h
+++ melody_manager.h
@@ -11,13 +11,16 @@
  * its duration than respect to the "tempo" 
  */
 struct NoteDuration{
-  int frequency;
-  int duration;
+  short frequency;
+  short duration;
 };
 
+/**
+ * This class aims to manage the melody in term of memory/storage and playing
+ */
 class Melody{
 public:
-  Melody(): nNotes(0), tempo(0){};
+  Melody(int pin = -1): nNotes(0), tempo(0){};
 
   /**
    * Load just 3 notes, to test the buzzer,
@@ -47,17 +50,49 @@ public:
     this->tempo=tempo;
   };
 
+  void setPin(int pin){
+    this->pin=pin;
+  }
+
   /**
    * Play the melody in a synchrounus (blocking) way.
    */
   void playSync();
+
+  /**
+   * Play the music using a thread, or something similar,
+   * to melody the melody while performing other action
+   */
+   void playAsync();
+
+   /**
+    * return if the solg is still playing, valid both async and sync
+    */
+    bool isPlaying();
   
 private:
+  int pin=-1;
   NoteDuration *nd;
   String title;
   int nNotes;
+  int maxLength = 1000;
   int tempo;
-  int debug=0;
+  /**
+   * 0, never played
+   * 1, playing
+   * 2, stopped
+   */
+  int state = 0;
+  const int debug = 0;
+
+  friend void changeFreq(Melody* melody);
+
+  /**
+   * Check is there the condition to play the melody, that is:
+   *  - a melody is loaded
+   *  - the pin is set
+   */
+  bool checkReadyness();
 };
 
 #endif
