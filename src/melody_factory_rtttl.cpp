8f8522be27fcb2d1ea4c5ca8f7e44e0039f13c68
Fabiano Riccardi <fabiano.riccardi@outlook.com>
2022-03-06 16:15:59 +0100

    add license

--- src/melody_factory_rtttl.cpp
+++ src/melody_factory_rtttl.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   This file is part of Melody Player, a library for Arduino             *
+ *   to play notes on piezoelectric buzzers.                               *
+ *                                                                         *
+ *   Copyright (C) 2021-2022  Fabiano Riccardi                             *
+ *                                                                         *
+ *   This library is free software; you can redistribute                   *
+ *   it and/or modify it under the terms of the GNU Lesser General Public  *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this library; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
 #include "melody_factory.h"
 #include "notes.h"
 
