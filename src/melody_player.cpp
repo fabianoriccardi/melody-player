fabianoriccardi/melody-player

commit f2e54f1164ddfd5c046396e87fad0453a1c846a4

refactor MelodyPlayer (add getCurrentComputedNote)

src/melody_player.cpp
@@ -34,23 +34,24 @@ void MelodyPlayer::play() {
 
   melodyState->advance();
   while (melodyState->getIndex() + melodyState->isSilence() < melodyState->melody.getLength()) {
-    NoteDuration note(melodyState->melody.getNote(melodyState->getIndex()));
+    NoteDuration computedNote = melodyState->getCurrentComputedNote();
     if (debug)
-      Serial.println(String("Playing: frequency:") + note.frequency + " duration:" + note.duration);
+      Serial.println(String("Playing: frequency:") + computedNote.frequency
+                     + " duration:" + computedNote.duration);
     if (melodyState->isSilence()) {
 #ifdef ESP32
       ledcWriteTone(pwmChannel, 0);
 #else
       noTone(pin);
 #endif
-      delay(0.3f * melodyState->melody.getTimeUnit() * note.duration);
+      delay(0.3f * computedNote.duration);
     } else {
 #ifdef ESP32
       ledcWriteTone(pwmChannel, note.frequency);
 #else
-      tone(pin, note.frequency);
+      tone(pin, computedNote.frequency);
 #endif
-      delay(melodyState->melody.getTimeUnit() * note.duration);
+      delay(computedNote.duration);
     }
     melodyState->advance();
   }
@@ -68,21 +69,20 @@ void changeTone(MelodyPlayer* player) {
   player->melodyState->advance();
   if (player->melodyState->getIndex() + player->melodyState->isSilence()
       < player->melodyState->melody.getLength()) {
-    NoteDuration note(player->melodyState->melody.getNote(player->melodyState->getIndex()));
-    int noteDur = player->melodyState->melody.getTimeUnit() * note.duration;
+    NoteDuration computedNote(player->melodyState->getCurrentComputedNote());
 
     float duration = player->melodyState->getRemainingDuration();
     if (duration > 0) {
       player->melodyState->resetRemainingDuration();
     } else {
       if (player->melodyState->isSilence()) {
-        duration = 0.3f * noteDur;
+        duration = 0.3f * computedNote.duration;
       } else {
-        duration = 1.0f * noteDur;
+        duration = computedNote.duration;
       }
     }
     if (player->debug)
-      Serial.println(String("Playing async: freq=") + note.frequency + " dur=" + duration
+      Serial.println(String("Playing async: freq=") + computedNote.frequency + " dur=" + duration
                      + " iteration=" + player->melodyState->getIndex());
 
     if (player->melodyState->isSilence()) {
@@ -101,7 +101,7 @@ void changeTone(MelodyPlayer* player) {
 #ifdef ESP32
       ledcWriteTone(player->pwmChannel, note.frequency);
 #else
-      tone(player->pin, note.frequency);
+      tone(player->pin, computedNote.frequency);
 #endif
 
 #ifdef ESP32

src/melody_player.h
@@ -182,6 +182,15 @@ private:
       return partialNoteReproduction;
     }
 
+    /**
+     * Get the current note, where the duration is the absolute time in milliseconds.
+     */
+    NoteDuration getCurrentComputedNote() const {
+      NoteDuration note = melody.getNote(getIndex());
+      note.duration = melody.getTimeUnit() * note.duration;
+      return note;
+    }
+
   private:
     bool first;
     bool silence;

commit fd1f59743179a1e3faaf4e8c2926aa844506b503

fix compile issue on esp32 (introduced in f2e54f1)

src/melody_player.cpp
@@ -47,7 +47,7 @@ void MelodyPlayer::play() {
       delay(0.3f * computedNote.duration);
     } else {
 #ifdef ESP32
-      ledcWriteTone(pwmChannel, note.frequency);
+      ledcWriteTone(pwmChannel, computedNote.frequency);
 #else
       tone(pin, computedNote.frequency);
 #endif
@@ -99,7 +99,7 @@ void changeTone(MelodyPlayer* player) {
 #endif
     } else {
 #ifdef ESP32
-      ledcWriteTone(player->pwmChannel, note.frequency);
+      ledcWriteTone(player->pwmChannel, computedNote.frequency);
 #else
       tone(player->pin, computedNote.frequency);
 #endif